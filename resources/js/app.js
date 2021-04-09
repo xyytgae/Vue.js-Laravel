a4283009161098b96c1aec2a6caabd069de49c55
xyytgae <xyytgae@yahoo.co.jp>
2021-04-10 00:11:18 +0900

    #1 Vueフロントエンド編

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,49 @@
+import VueRouter from 'vue-router'
+import HeaderComponent from './components/HeaderComponent'
+import TaskListComponent from './components/TaskListComponent'
+import TaskCreateComponent from './components/TaskCreateComponent'
+import TaskShowComponent from './components/TaskShowComponent'
+import TaskEditComponent from './components/TaskEditComponent'
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue').default;
+window.Vue = require('vue').default
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent,
+        },
+        {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent,
+        },
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true,
+        },
+        {
+            path: '/tasks/:taskId/edit',
+            name: 'task.edit',
+            component: TaskEditComponent,
+            props: true,
+        },
+    ],
+})
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +56,13 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// import Vue from 'vue'
+
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default,
+)
+Vue.component('header-component', HeaderComponent)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +72,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-});
+    router,
+})
